0e8d0c3aa942471b792d26edc51fc3108c2eff12
Zack Stout <zackstout@gmail.com>
2018-02-03 19:50:55 -0600

    whoops it's actually hiphop and sleepingalone that are whiffing

--- scripts/sleeping-alone.py
+++ scripts/sleeping-alone.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 from matplotlib import style
 import numpy as np
 # from os import path
+style.use('ggplot')
 
 with open('csvs/sleeping-alone-data.csv', 'rb') as f:
   contents = f.read()
